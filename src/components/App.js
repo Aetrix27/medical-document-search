d91a460add1b1224ea255606a272941392daa3e0
David <davedogg323@gmail.com>
2022-02-24 23:57:19 -0800

    Setup navbar

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom'
+
+import Navbar from './Navbar.js'
+import './App.css';
+
+
+function App(){
+    return (
+        <div className="App">
+                Medical Documents Search
+        <Navbar />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import './Navbar.css'
+// src/Title.js
+import { NavLink } from 'react-router-dom'
+import React from 'react'
+
+function Navbar(){
+    return (
+        <div className="Title">
+        <h1>Medical Documents Search</h1>
+                <div className="Title-Subtitle">Search for a document!</div>
+
+        <div>
+            <NavLink 
+            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+            to="/">Home</NavLink>
+            <NavLink 
+            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+            to="/documents">Documents</NavLink>
+            
+        </div>
+        </div>
+    )
+}
+
+  export default Navbar
